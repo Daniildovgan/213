7ab78320b51a875e242b2e393be7befe39cd3957
Daniildovgan <155999529+Daniildovgan@users.noreply.github.com>
2024-04-23 17:24:47 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Не питайте, хто йде шляхом, " << std::endl;
+    std::cout << "Ідіть самі, будьте дорогами," << std::endl;
+    std::cout << "Будьте сонцем і будьте вітром, " << std::endl;
+    std::cout << "Будьте воля в революціях." << std::endl;
+    std::cout << "- Тарас Шевченко" << std::endl;
+    return 0;
+}
